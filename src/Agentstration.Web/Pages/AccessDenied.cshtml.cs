99fec911951f301c7ca8834eca0f5a6f209c91db
gbaudrit <gbaudrit.public@gmail.com>
2026-08-25 12:09:14 +0200

    Fix Razor page import ordering

--- src/Agentstration.Web/Pages/AccessDenied.cshtml.cs
+++ src/Agentstration.Web/Pages/AccessDenied.cshtml.cs
@@ -1,5 +1,5 @@
-using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Agentstration.Web.Pages;
 
--- src/Agentstration.Web/Pages/Bootstrap.cshtml.cs
+++ src/Agentstration.Web/Pages/Bootstrap.cshtml.cs
@@ -2,10 +2,10 @@ using System.ComponentModel.DataAnnotations;
 using Agentstration.Security.AspNetCoreIdentity;
 using Agentstration.Web.Configuration;
 using Agentstration.Web.Security;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.Extensions.Options;
 
 namespace Agentstration.Web.Pages;
