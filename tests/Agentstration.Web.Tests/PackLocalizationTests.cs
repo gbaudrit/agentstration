08bc4f7d2c961cb648e270c2e32ee8aee2324d05
gbaudrit <gbaudrit.public@gmail.com>
2026-08-29 12:01:52 +0200

    Localize Pack composition

---
+++ tests/Agentstration.Web.Tests/PackLocalizationTests.cs
@@ -0,0 +1,34 @@
+using Agentstration.Web.Components.Pages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Localization;
+using System.Globalization;
+
+namespace Agentstration.Web.Tests;
+
+[TestClass]
+[DoNotParallelize]
+public sealed class PackLocalizationTests
+{
+    [TestMethod]
+    public void CatalogUsesTheSelectedFrenchCulture()
+    {
+        var originalCulture = CultureInfo.CurrentCulture;
+        var originalUiCulture = CultureInfo.CurrentUICulture;
+        try
+        {
+            CultureInfo.CurrentCulture = CultureInfo.GetCultureInfo("fr-FR");
+            CultureInfo.CurrentUICulture = CultureInfo.GetCultureInfo("fr-FR");
+            var services = new ServiceCollection().AddLogging().AddLocalization(options => options.ResourcesPath = "Resources").BuildServiceProvider();
+            var strings = services.GetRequiredService<IStringLocalizer<PackStrings>>();
+
+            Assert.AreEqual("Créer un projet de Pack", strings["CreatePackProject"].Value);
+            Assert.AreEqual("Fournisseur de modèles", strings["Binding.ModelProvider"].Value);
+            Assert.AreEqual("Retirer agent du Pack", strings["RemoveFromPackLabel", "agent"].Value);
+        }
+        finally
+        {
+            CultureInfo.CurrentCulture = originalCulture;
+            CultureInfo.CurrentUICulture = originalUiCulture;
+        }
+    }
+}
