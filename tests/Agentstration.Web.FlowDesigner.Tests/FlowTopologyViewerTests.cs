eba42ef6badc0a2ddd5d1cf72b2f02214420a2eb
gbaudrit <gbaudrit.public@gmail.com>
2026-09-04 01:53:11 +0200

    feat(console): replay flow run topology sequence

--- tests/Agentstration.Web.FlowDesigner.Tests/FlowTopologyViewerTests.cs
+++ tests/Agentstration.Web.FlowDesigner.Tests/FlowTopologyViewerTests.cs
@@ -118,6 +118,33 @@ public sealed class FlowTopologyViewerTests
     }
 
     [TestMethod]
+    public void ViewerExposesReplayControlAndDisablesItDuringPlayback()
+    {
+        using var context = new BunitContext();
+        context.Services.AddLocalization(options => options.ResourcesPath = "Resources");
+        var replayRequests = 0;
+        var graph = new FlowTopologyGraph(
+            [new("step:input", "input", "Input", "input", 0, 0)],
+            [],
+            "graph",
+            "Read-only topology");
+        var rendered = context.Render<FlowTopologyViewer>(parameters => parameters
+            .Add(component => component.Graph, graph)
+            .Add(component => component.ReplayRequested, () => replayRequests++));
+
+        var replay = rendered.Find("button.replay-toggle");
+        Assert.IsFalse(replay.HasAttribute("disabled"));
+        replay.Click();
+        Assert.AreEqual(1, replayRequests);
+
+        var playing = context.Render<FlowTopologyViewer>(parameters => parameters
+            .Add(component => component.Graph, graph)
+            .Add(component => component.ReplayRequested, () => replayRequests++)
+            .Add(component => component.ReplayInProgress, true));
+        Assert.IsTrue(playing.Find("button.replay-toggle").HasAttribute("disabled"));
+    }
+
+    [TestMethod]
     public void ViewerRoutesOppositeHandoffsInParallelAndUsesIntrinsicCanvasWidth()
     {
         using var context = new BunitContext();
