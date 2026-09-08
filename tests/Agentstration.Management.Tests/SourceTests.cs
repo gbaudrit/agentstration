50b1eb6354c68450bf8198297815606481a48373
gbaudrit <gbaudrit.public@gmail.com>
2026-09-08 23:12:07 +0200

    style(sources): order test imports

--- tests/Agentstration.Management.Tests/SourceTests.cs
+++ tests/Agentstration.Management.Tests/SourceTests.cs
@@ -1,6 +1,6 @@
+using System.IO.Compression;
 using System.Net;
 using System.Net.Http.Json;
-using System.IO.Compression;
 using System.Security.Cryptography;
 using System.Text;
 using Agentstration.Infrastructure.Sources;
